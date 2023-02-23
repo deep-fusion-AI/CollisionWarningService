872153b36fcf1f6c37c42fba8b1cb66dbff63c60
Petr Klepárník <ikleparnik@fit.vutbr.cz>
2023-02-23 22:16:17 +0100

    #6: Wrap YOLODetector to YOLODetectorWorker class, fix era_5g_client and era_5g_interface packages import, fcw_example.py is functional without 5g_era stuff

--- client.py
+++ client.py
@@ -1,20 +1,27 @@
-import traceback
-import math
-import time
-from threading import Thread, Event
-import cv2
-import signal
-from queue import Queue
+from __future__ import annotations
+
 import logging
+import math
 import os
+import signal
+import time
+import traceback
+from queue import Queue
+from threading import Event, Thread
+from types import FrameType
+from typing import Any, Dict, Optional
 from pathlib import Path
+
+import cv2
+import numpy as np
 import yaml
-import json
 
-from era_5g_netapp_client.client import FailedToConnect, NetAppClient
+from era_5g_client.client import NetAppClient
+from era_5g_client.exceptions import FailedToConnect
 
-image_storage = dict()
-results_storage = Queue()
+image_storage: Dict[str, np.ndarray] = dict()
+results_storage: Queue[Dict[str, Any]] = Queue()
+stopped = False
 
 DEBUG_PRINT_SCORE = False  # useful for FPS detector
 DEBUG_PRINT_DELAY = False  # prints the delay between capturing image and receiving the results
@@ -35,28 +42,28 @@ camera_config = Path("videos/video3.yaml")
 
 
 class ResultsViewer(Thread):
-
-    def __init__(self, **kw):
+    def __init__(self, **kw) -> None:
         super().__init__(**kw)
         self.stop_event = Event()
 
-    def stop(self):
+    def stop(self) -> None:
         self.stop_event.set()
 
-    def run(self):
+    def run(self) -> None:
         logging.info("Thread %s: starting", self.name)
         while not self.stop_event.is_set():
             if not results_storage.empty():
                 results = results_storage.get(timeout=1)
-                timestamp = int(results['timestamp'])
+                timestamp_str = results["timestamp"]
+                timestamp = int(timestamp_str)
                 if DEBUG_PRINT_DELAY:
                     time_now = math.floor(time.time() * 100)
                     print(f"{(time_now - timestamp) / 100.}s delay")
                 try:
-                    frame = image_storage.pop(timestamp)
+                    frame = image_storage.pop(timestamp_str)
                     detections = results["detections"]
                     for d in detections:
-                        score = d["score"]
+                        score = float(d["score"])
                         if DEBUG_PRINT_SCORE and score > 0:
                             print(score)
                         cls_name = d["class_name"]
@@ -64,8 +71,16 @@ class ResultsViewer(Thread):
                         x1, y1, x2, y2 = [int(coord) for coord in d["bbox"]]
                         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
                         font = cv2.FONT_HERSHEY_SIMPLEX
-                        cv2.putText(frame, f"{cls_name} ({score * 100:.0f})%", (x1, y1 - 5), font, .5, (0, 255, 0), 1,
-                                    cv2.LINE_AA)
+                        cv2.putText(
+                            frame,
+                            f"{cls_name} ({score * 100:.0f})%",
+                            (x1, y1 - 5),
+                            font,
+                            0.5,
+                            (0, 255, 0),
+                            1,
+                            cv2.LINE_AA,
+                        )
                     try:
                         cv2.imshow("Results", frame)
                         cv2.waitKey(1)
@@ -76,37 +91,37 @@ class ResultsViewer(Thread):
                     print(ex)
 
 
-def get_results(results: str):
-    """
-    Callback which process the results from the NetApp
+def get_results(results: Dict[str, Any]) -> None:
+    """Callback which process the results from the NetApp
 
     Args:
         results (str): The results in json format
     """
 
     print(results)
-    if 'timestamp' in results:
+    if "timestamp" in results:
         results_storage.put(results, block=False)
     pass
 
 
-def main():
-    """
-    Creates the client class and starts the data transfer
-    """
+def main() -> None:
+    """Creates the client class and starts the data transfer."""
 
     results_viewer = ResultsViewer(name="test_client_http_viewer", daemon=True)
     results_viewer.start()
+
+    logging.getLogger().setLevel(logging.INFO)
 
     client = None
     global stopped
     stopped = False
 
-    def signal_handler(sig, frame):
+    def signal_handler(sig: int, frame: Optional[FrameType]) -> None:
         global stopped
         stopped = True
         results_viewer.stop()
         print(f"Terminating ({signal.Signals(sig).name})...")
+
     signal.signal(signal.SIGTERM, signal_handler)
     signal.signal(signal.SIGINT, signal_handler)
 
@@ -114,7 +129,7 @@ def main():
         config_dict = yaml.safe_load(config.open())
 
         # creates the NetApp client with gstreamer extension
-        client = NetAppClient(None, None, None, None, True, get_results, False, False, NETAPP_ADDRESS, NETAPP_PORT)
+        client = NetAppClient("", "", "", "", True, get_results, False, False, NETAPP_ADDRESS, NETAPP_PORT)
         # register the client with the NetApp
         client.register({"config": config_dict})
         if FROM_SOURCE:
@@ -134,8 +149,9 @@ def main():
             if not ret:
                 break
             resized = cv2.resize(frame, (640, 480), interpolation=cv2.INTER_AREA)
-            image_storage[timestamp] = resized
-            client.send_image(resized, str(timestamp), 5)
+            timestamp_str = str(timestamp)
+            image_storage[timestamp_str] = resized
+            client.send_image(resized, timestamp_str, 5)
 
     except FailedToConnect as ex:
         print(f"Failed to connect to server ({ex})")
@@ -150,5 +166,5 @@ def main():
             client.disconnect()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- yolo_detector.py
+++ yolo_detector.py
@@ -12,38 +12,30 @@ https://github.com/ultralytics/yolov5/blob/master/data/coco128.yaml
 
 """
 
+from typing import Iterable
+
 import cv2
 import numpy as np
 import torch
 from shapely.geometry import box
-from typing import Iterable
-import flask_socketio
-from queue import Queue
 
-import detection
 from detection import ObjectObservation
 
-from era_5g_object_detection_common.image_detector import ImageDetector, ImageDetectorInitializationFailed
-from era_5g_object_detection_standalone.worker import Worker
 
-
-class YOLODetector(Worker, ImageDetector):
+class YOLODetector:
     default_classes = [
         "person", "bicycle", "car", "motorcycle", "bus", "truck"
     ]
 
-    def __init__(self,
-                 image_queue: Queue,
-                 app,
-                 model: str = "yolov5l6",
-                 classes: Iterable[str] = None,
-                 max_size: int = 640,
-                 min_score: float = 0.3,
-                 filter_in_frame: bool = True,
-                 min_area: float = None,
-                 **kw
-                 ):
-        super().__init__(image_queue=image_queue, app=app, **kw)
+    def __init__(
+        self,
+        model: str = "yolov5l6",
+        classes: Iterable[str] = None,
+        max_size: int = 640,
+        min_score: float = 0.3,
+        filter_in_frame: bool = True,
+        min_area: float = None
+        ):
         self.model = torch.hub.load("ultralytics/yolov5", model, pretrained=True)
         self.model.agnostic = False
         self.model.iou = 0.7
@@ -54,8 +46,7 @@ class YOLODetector(Worker, ImageDetector):
             name_idx = dict(((name, class_id) for class_id, name in self.model.names.items()))
             # List class_id specified by names in classes passed as parameter, ignoring unknown classes
             self.model.classes = [name_idx[nm] for nm in classes if
-                                  nm in name_idx] or None  # ... or None - in case og empty list leave None value not
-            # empty list
+                                  nm in name_idx] or None  # ... or None - in case og empty list leave None value not empty list
         self.model.conf = min_score
         self.max_size = max_size
         self.filter_in_frame = filter_in_frame
@@ -64,17 +55,15 @@ class YOLODetector(Worker, ImageDetector):
     @staticmethod
     def from_dict(d: dict) -> "YOLODetector":
         return YOLODetector(
-                image_queue=None,
-                app=None,
-                model=d.get("model", "yolov5n6"),
-                classes=d.get("classes"),
-                max_size=d.get("max_size", 1024),
-                min_score=d.get("min_score", 0.3),
-                filter_in_frame=d.get("filter_in_frame", False),
-                min_area=d.get("min_area"),
+            model=d.get("model", "yolov5n6"),
+            classes=d.get("classes"),
+            max_size=d.get("max_size", 1024),
+            min_score=d.get("min_score", 0.3),
+            filter_in_frame=d.get("filter_in_frame", False),
+            min_area=d.get("min_area"),
         )
 
-    def process_image(self, image):
+    def detect(self, image):
         h, w = image.shape[:2]
         if max(h, w) > self.max_size:
             scale = max(h, w) / self.max_size  # shape (1080, 1920), max_size=960 -> scale=1920/960 = 2
@@ -98,9 +87,9 @@ class YOLODetector(Worker, ImageDetector):
         # Generator of object instances
         all_dets = (
             ObjectObservation(
-                    geometry=geometry,
-                    score=score,
-                    label=label,
+                geometry=geometry,
+                score=score,
+                label=label,
             )
             for geometry, label, score in zip(geometries, labels, scores)
         )
@@ -110,37 +99,9 @@ class YOLODetector(Worker, ImageDetector):
             is_in_frame = lambda d: d.is_in_frame((h, w), margin=10)
             all_dets = filter(is_in_frame, all_dets)
 
-        # Filter objects with sufficient size
+        # Filter objects with suficient size
         if self.min_area is not None and self.min_area > 0:
             sufficient_size = lambda d: d.geometry.area > self.min_area
             all_dets = filter(sufficient_size, all_dets)
 
         return list(all_dets)
-
-    def publish_results(self, results, metadata):
-        """
-        Publishes the results to the robot
-
-        Args:
-            metadata (_type_): NetApp-specific metadata related to processed image.
-            results (_type_): The results of the detection.
-        """
-        detections = list()
-
-        if results is not None:
-            for result in results:
-                det = dict()
-                det["bbox"] = result.bounds()
-                det["score"] = result.score
-                det["class"] = result.label
-                det["class_name"] = self.model.names[result.label]
-
-                detections.append(det)
-
-            r = {"timestamp": metadata["timestamp"],
-                 "detections": detections}
-
-            # use the flask app to return the results
-            with self.app.app_context():
-                # print(f"publish_results to: {metadata['websocket_id']} flask_socketio.send: {r}")
-                flask_socketio.send(r, namespace='/results', to=metadata["websocket_id"])
