996e711a3aabcb7551c9b87c09e8c418f63c4671
deep-fusion-AI <dongho@deep-fusion.com>
2024-07-03 16:57:58 +0900

    updated

--- fcw-core/fcw_core/run.sh
+++ fcw-core/fcw_core/run.sh
@@ -1,3 +1,4 @@
-python3 fcw_example.py --viz -t 150 -c ../../config/config.yaml --camera ../../videos/video3.yaml ../../videos/video3.mp4
+python3 fcw_example.py --viz -t 150 -c ../../config/config.yaml --camera ../../videos/dfai.yaml ../../videos/dfai.mp4
+#python3 fcw_example.py --viz -t 150 -c ../../config/config.yaml --camera ../../videos/video3.yaml ../../videos/video3.mp4
 
 
--- install_local_pkg.sh
+++ install_local_pkg.sh
@@ -4,17 +4,20 @@ LOCAL_PYTHON3_SITE_PKG="/home/autoroad/.local/lib/python3.10/site-packages"
 FCW_CORE=$PWD"/fcw-core/fcw_core"
 FCW_SERVICE=$PWD"/fcw-service/fcw_service"
 FCW_CLIENT=$PWD"/fcw-client/fcw_client"
+FCW_UTILS=$PWD"/fcw-core-utils/fcw_core_utils"
 
 #ln -s /home/dfai/dongho/CollisionWarningService/fcw-core/fcw_core ./
 
 echo $FCW_CORE
 echo $FCW_SERVICE
 echo $FCW_CLIENT
+echo $FCW_UTILS
 
 # make link
 ln -s $FCW_CORE $LOCAL_PYTHON3_SITE_PKG
 ln -s $FCW_SERVICE $LOCAL_PYTHON3_SITE_PKG
 ln -s $FCW_CLIENT $LOCAL_PYTHON3_SITE_PKG
+ln -s $FCW_UTILS $LOCAL_PYTHON3_SITE_PKG
 
 
 # check
